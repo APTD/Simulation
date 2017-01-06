309a2752bbf3035dfa11997294d4d8f41f829585
Philipp Kraus <philipp.kraus@flashpixx.de>
2017-01-06 15:39:40 +0100

    add plan data

--- src/main/webapp/com/github/aptd/simulation/html/script.js
+++ src/main/webapp/com/github/aptd/simulation/html/script.js
@@ -14,9 +14,25 @@ jQuery(function() {
 
                 Asimov.ajax( "/agent/" + jQuery( this ).text() + "/view" )
                 .done( function(po) {
+
                     var lo_beliefs = jQuery("<ul></ul>");
                     po.belief.forEach( function(i){ lo_beliefs.append( jQuery("<li></li>").text( Asimov.literal(i) ) ); });
                     jQuery("#agentdetail_belief").empty().append( lo_beliefs );
+
+                    var lo_plans = jQuery("<ul></ul>");
+                    po.plan.forEach( function(i){ lo_plans.append( jQuery("<li></li>").text( i.trigger + Asimov.literal(i.literal) + " [Success: " + i.success + "/ Fail: " + i.fail + "]" ) ); });
+                    jQuery("#agentdetail_plan").empty().append( lo_plans );
+
+                    jQuery("#agentdetail_main")
+                        .empty()
+                        .append(
+                            jQuery( "<ul></ul>" )
+                                .append( "<li><strong>ID:</strong> " + po.id + "</li>" )
+                                .append( "<li><strong>Sleeping Cycles:</strong> " + po.sleeping + "</li>" )
+                                .append( "<li><strong>Cycle:</strong> " + po.cycle + "</li>" )
+                        );
+
+                    console.log(po);
                 });
 
             });
