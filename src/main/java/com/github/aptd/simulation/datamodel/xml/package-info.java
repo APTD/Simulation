1c93591436bb6a5d99ff9373006a1809134bbf22
Sebastian Albert <albert@math.uni-goettingen.de>
2017-07-10 11:33:54 +0200

    custom package-info for JAXB classes with preferred namespace prefixes

--- src/main/java/com/github/aptd/simulation/datamodel/xml/package-info.java
+++ src/main/java/com/github/aptd/simulation/datamodel/xml/package-info.java
@@ -1,3 +1,39 @@
-/**
- * Created by seb on 07.07.17.
+/*
+ * @cond LICENSE
+ * ######################################################################################
+ * # LGPL License                                                                       #
+ * #                                                                                    #
+ * # This file is part of the Asimov - Agentbased Passenger Train Delay                 #
+ * # This program is free software: you can redistribute it and/or modify               #
+ * # it under the terms of the GNU Lesser General Public License as                     #
+ * # published by the Free Software Foundation, either version 3 of the                 #
+ * # License, or (at your option) any later version.                                    #
+ * #                                                                                    #
+ * # This program is distributed in the hope that it will be useful,                    #
+ * # but WITHOUT ANY WARRANTY; without even the implied warranty of                     #
+ * # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                      #
+ * # GNU Lesser General Public License for more details.                                #
+ * #                                                                                    #
+ * # You should have received a copy of the GNU Lesser General Public License           #
+ * # along with this program. If not, see http://www.gnu.org/licenses/                  #
+ * ######################################################################################
+ * @endcond
  */
+
+/**
+ * auto-generated package
+ *
+ * checkstyle is strange
+ * @since
+ */
+
+@javax.xml.bind.annotation.XmlSchema(
+    namespace = "https://raw.githubusercontent.com/APTD/Simulation/master/src/main/xsd",
+    xmlns = {
+        @XmlNs( namespaceURI = "https://raw.githubusercontent.com/APTD/Simulation/master/src/main/xsd", prefix = "asimov" ),
+        @XmlNs( namespaceURI = "http://www.railml.org/schemas/2016", prefix = "rail" )
+    },
+    elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED )
+package com.github.aptd.simulation.datamodel.xml;
+
+import javax.xml.bind.annotation.XmlNs;
